30e8d3acd77835b856653310b31ff80593de782c
MRootxx <dr.rootvb@gmail.com>
2026-02-05 03:19:06 +0300

    Fix activity logs route name in sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -119,7 +119,7 @@
                                 <x-sidebar-link :href="route('admin.reviews')" :active="request()->routeIs('admin.reviews')" icon="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z">
                                     {{ __('المراجعات') }}
                                 </x-sidebar-link>
-                                <x-sidebar-link :href="route('activity-logs')" :active="request()->routeIs('activity-logs')" icon="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01">
+                                <x-sidebar-link :href="route('admin.activity-logs')" :active="request()->routeIs('admin.activity-logs')" icon="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01">
                                     {{ __('سجل النشاطات') }} 🛡️
                                 </x-sidebar-link>
                             @endif
